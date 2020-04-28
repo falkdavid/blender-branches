0c2fa94510cb9bbf1f998eec7312acc3fa4cef78
Falk David <falkdavid@gmx.de>
2020-04-28 11:55:42 +0200

    Remove header declarations

--- source/blender/blenkernel/BKE_gpencil_geom.h
+++ source/blender/blenkernel/BKE_gpencil_geom.h
@@ -132,17 +132,6 @@ struct bGPDstroke *BKE_gpencil_stroke_perimeter_from_view(const struct bContext 
                                                           struct bGPDstroke *gps,
                                                           int subdivisions);
 
-struct bGPDstroke *BKE_gpencil_stroke_perimeter_from_proj_mat(const struct bGPdata *gpd,
-                                                              const struct bGPDlayer *gpl,
-                                                              struct bGPDstroke *gps,
-                                                              const float proj_mat[4][4],
-                                                              int subdivisions);
-
-// void BKE_gpencil_stroke_difference(struct bGPDstroke *gps_A, struct bGPDstroke *gps_B);
-
-bool BKE_gpencil_stroke_resolve_self_overlapp(const struct RegionView3D *rv3d,
-                                              struct bGPDstroke *gps);
-
 #ifdef __cplusplus
 }
 #endif
