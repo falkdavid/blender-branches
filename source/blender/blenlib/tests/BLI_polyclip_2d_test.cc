b1b7eb1176f488892f896fe70b89072d7253f1a5
Falk David <falkdavid@gmx.de>
2020-09-12 13:15:27 +0200

    PolyClip: Add files

---
+++ source/blender/blenlib/tests/BLI_polyclip_2d_test.cc
@@ -0,0 +1,9 @@
+/* Apache License, Version 2.0 */
+
+#include "testing/testing.h"
+
+#include "BLI_polyclip_2d.hh"
+
+namespace blender::polyclip::tests {
+
+}  // namespace blender::polyclip::tests
