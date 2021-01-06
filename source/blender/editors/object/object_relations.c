9ae20d7825b1e55cce1606f1a760d44d076bdd29
Falk David <falkdavid@gmx.de>
2021-01-06 11:51:00 +0100

    Fix T84420: Linking regular materials to gpencil
    When using "Make Links"->"Materials" regular materials could be linked
    onto grease pencil objects. This caused a number of issues.
    
    The fix changes the `allow_make_links_data` function to make sure that
    if one object is of type `OB_GPENCIL`, the other has to be aswell.
    
    Differential Revision: https://developer.blender.org/D10014

--- source/blender/editors/object/object_relations.c
+++ source/blender/editors/object/object_relations.c
@@ -1567,7 +1567,10 @@ static bool allow_make_links_data(const int type, Object *ob_src, Object *ob_dst
       }
       break;
     case MAKE_LINKS_MATERIALS:
-      if (OB_TYPE_SUPPORT_MATERIAL(ob_src->type) && OB_TYPE_SUPPORT_MATERIAL(ob_dst->type)) {
+      if (OB_TYPE_SUPPORT_MATERIAL(ob_src->type) && OB_TYPE_SUPPORT_MATERIAL(ob_dst->type) &&
+          /* T84420: Linking non-gpencil materials to a gp object causes issues. We make sure that
+           * if one of the objects is a gpencil object, the other must be as well. */
+          !((ob_src->type == OB_GPENCIL) ^ (ob_dst->type == OB_GPENCIL))) {
         return true;
       }
       break;
